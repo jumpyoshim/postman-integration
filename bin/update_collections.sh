09720e00bf4ac605a8032e1287037a42d9315a9a
jumpyoshim <jumpei@hicustomer.jp>
2019-12-16 23:32:17 +0900

    update

--- bin/update_collections.sh
+++ bin/update_collections.sh
@@ -2,17 +2,16 @@
 
 if [ -z "$POSTMAN_API_KEY" ]
 then
-      echo "POSTMAN_API_KEY is empty"
-      exit 1
+  echo "POSTMAN_API_KEY is empty"
+  exit 1
 fi
 
 set -e
 
-FILES="/tmp/*"
+FILES="/tmp/*.json"
 for f in $FILES
 do
-  ID=$(cat < "$f" | python3 -c "import sys, json; print(json.load(sys.stdin)['collection']['info']['_postman_id'])")
-
+  ID=$(python3 -c "import sys, json; print(json.load(sys.stdin)['collection']['info']['_postman_id'])" < "$f")
   curl --location --request PUT https://api.getpostman.com/collections/"$ID" \
   --header "Content-Type: application/json" \
   --header "X-Api-Key: $POSTMAN_API_KEY" \
